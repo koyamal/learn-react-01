8002203a4842d14ca3d8bba8c6fea65990df3ae5
koyamal <74910401+koyamal@users.noreply.github.com>
2022-09-18 17:31:18 +0900

    Use divided input in "ColorfulMessage.jsx"

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
+  const { color, message } = props;
   const contentStyle = {
-    color: props.color,
+    color: color,
     fontSize: "18px"
   };
-  return <p style={contentStyle}>{props.message}</p>;
+  return <p style={contentStyle}>{message}</p>;
 };
 
 export default ColorfulMessage;
