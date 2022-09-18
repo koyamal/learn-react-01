4134a8092bb3085c5c8cd9897fffa922bd100186
koyamal <74910401+koyamal@users.noreply.github.com>
2022-09-18 17:04:43 +0900

    Use component of react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>hello!!</h1>
+      <p>How are you?</p>
+    </>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom";
+import App from "./App";
 
-const App = () => {
-  return (
-    <>
-      <h1>hello!!</h1>
-      <p>How are you?</p>
-    </>
-  );
-};
+// const App = () => {
+//   return (
+//     <>
+//       <h1>hello!!</h1>
+//       <p>How are you?</p>
+//     </>
+//   );
+// };
 
 ReactDom.render(<App />, document.getElementById("root"));
