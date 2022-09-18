e0a2bed287235b5e7d19035d1871ffee01fb8ff2
koyamal <74910401+koyamal@users.noreply.github.com>
2022-09-18 17:37:44 +0900

    Use useState [num, setNum] = useState(0);

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 import ColorfulMessageChildren from "./components/ColorfulMessageChildren";
 
 const App = () => {
-  const onClickButton = () => {
-    alert();
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1 style={{ color: "red" }}>hello!!</h1>
       <ColorfulMessage color="blue" message="How are you?" />
       <ColorfulMessage color="pink" message="I'm fine!" />
       <ColorfulMessageChildren color="blue">Good!!</ColorfulMessageChildren>
-      <button onClick={onClickButton}>Button</button>
+      <button onClick={onClickCountUp}>Count Up</button>
+      <p>{num}</p>
     </>
   );
 };
