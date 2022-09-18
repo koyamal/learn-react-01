2b4dd529d9809ae98c5844d33d7fbc4613a95274
koyamal <74910401+koyamal@users.noreply.github.com>
2022-09-18 17:08:43 +0900

    Rename App.js to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => {
+    alert();
+  };
   return (
     <>
       <h1>hello!!</h1>
       <p>How are you?</p>
+      <button onClick={onClickButton}>Button</button>
     </>
   );
 };
